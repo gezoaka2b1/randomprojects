16497526a9cbc013a253cc1fd932e573704dbf29
gezoaka2b1 <152023743+gezoaka2b1@users.noreply.github.com>
2023-11-25 20:32:33 +0300

    Create uravgmathgrade.cpp

---
+++ uravgmathgrade.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e,avg;
+    cout << "this will calculate ur avg grade from ur first 5 grades at math" << endl;
+    cout << "ur first grade" << endl;
+    cin>>a;
+    cout << "ur second grade" << endl;
+    cin>>b;
+    cout << "ur third grade" << endl;
+    cin>>c;
+    cout << "ur fourth grade" << endl;
+    cin>>d;
+    cout << "ur fifth grade" << endl;
+    cin>>e;
+    cout << "ur avg is " <<(a+b+c+d+e)/5<< endl;
+    return 0;
+}
