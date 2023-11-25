9f13f54c5ca928f99fc1f0b5c9dea45f5955510a
gezoaka2b1 <152023743+gezoaka2b1@users.noreply.github.com>
2023-11-25 20:56:14 +0300

    Create todolistmanager.cpp

---
+++ todolistmanager.cpp
@@ -0,0 +1,409 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "u will have 10 lines on ur list" << endl;
+    string a,b,c,d,e,f,g,h,i,x,endig;
+    cout << "ur first task" << endl;
+    cin>>a;
+    cout << "ur second task" << endl;
+    cin>>b;
+    cout << "ur third task" << endl;
+    cin>>c;
+    cout << "ur fourth task" << endl;
+    cin>>d;
+    cout << "ur fifth task" << endl;
+    cin>>e;
+    cout << "ur 6th task" << endl;
+    cin>>f;
+    cout << "ur 7th task" << endl;
+    cin>>g;
+    cout << "ur 8th task" << endl;
+    cin>>h;
+    cout << "ur 9th task" << endl;
+    cin>>i;
+    cout << "ur 10th task" << endl;
+    cin>>x;
+    cout <<a<< endl;
+    cout <<b<< endl;
+    cout <<c<< endl;
+    cout <<d<< endl;
+    cout <<e<< endl;
+    cout <<f<< endl;
+    cout <<g<< endl;
+    cout <<h<< endl;
+    cout <<i<< endl;
+    cout <<x<< endl;
+    cout <<"what task did u finish?(a b c d e f g h i x)"<< endl;
+    cin>>endig;
+    if (endig=="a")
+        a="finished";
+    else if (endig=="b")
+        b="finished";
+    else if (endig=="c")
+        c="finished";
+    else if (endig=="d")
+        d="finished";
+    else if (endig=="e")
+        e="finished";
+    else if (endig=="f")
+        f="finished";
+    else if (endig=="g")
+        g="finished";
+    else if (endig=="h")
+        h="finished";
+    else if (endig=="i")
+        i="finished";
+    else if (endig=="x")
+        x="finished";
+    else
+    {
+        cout<<"hmmmm that is not valid"<< endl;
+        terminate();
+    }
+    cout <<a<< endl;
+    cout <<b<< endl;
+    cout <<c<< endl;
+    cout <<d<< endl;
+    cout <<e<< endl;
+    cout <<f<< endl;
+    cout <<g<< endl;
+    cout <<h<< endl;
+    cout <<i<< endl;
+    cout <<x<< endl;
+    cout <<"what task did u finish?(a b c d e f g h i x)"<< endl;
+    cin>>endig;
+    if (endig=="a")
+        a="finished";
+    else if (endig=="b")
+        b="finished";
+    else if (endig=="c")
+        c="finished";
+    else if (endig=="d")
+        d="finished";
+    else if (endig=="e")
+        e="finished";
+    else if (endig=="f")
+        f="finished";
+    else if (endig=="g")
+        g="finished";
+    else if (endig=="h")
+        h="finished";
+    else if (endig=="i")
+        i="finished";
+    else if (endig=="x")
+        x="finished";
+    else
+    {
+        cout<<"hmmmm that is not valid"<< endl;
+        terminate();
+    }
+    cout <<a<< endl;
+    cout <<b<< endl;
+    cout <<c<< endl;
+    cout <<d<< endl;
+    cout <<e<< endl;
+    cout <<f<< endl;
+    cout <<g<< endl;
+    cout <<h<< endl;
+    cout <<i<< endl;
+    cout <<x<< endl;
+    cout <<"what task did u finish?(a b c d e f g h i x)"<< endl;
+    cin>>endig;
+    if (endig=="a")
+        a="finished";
+    else if (endig=="b")
+        b="finished";
+    else if (endig=="c")
+        c="finished";
+    else if (endig=="d")
+        d="finished";
+    else if (endig=="e")
+        e="finished";
+    else if (endig=="f")
+        f="finished";
+    else if (endig=="g")
+        g="finished";
+    else if (endig=="h")
+        h="finished";
+    else if (endig=="i")
+        i="finished";
+    else if (endig=="x")
+        x="finished";
+    else
+    {
+        cout<<"hmmmm that is not valid"<< endl;
+        terminate();
+    }
+    cout <<a<< endl;
+    cout <<b<< endl;
+    cout <<c<< endl;
+    cout <<d<< endl;
+    cout <<e<< endl;
+    cout <<f<< endl;
+    cout <<g<< endl;
+    cout <<h<< endl;
+    cout <<i<< endl;
+    cout <<x<< endl;
+    cout <<"what task did u finish?(a b c d e f g h i x)"<< endl;
+    cin>>endig;
+    if (endig=="a")
+        a="finished";
+    else if (endig=="b")
+        b="finished";
+    else if (endig=="c")
+        c="finished";
+    else if (endig=="d")
+        d="finished";
+    else if (endig=="e")
+        e="finished";
+    else if (endig=="f")
+        f="finished";
+    else if (endig=="g")
+        g="finished";
+    else if (endig=="h")
+        h="finished";
+    else if (endig=="i")
+        i="finished";
+    else if (endig=="x")
+        x="finished";
+    else
+    {
+        cout<<"hmmmm that is not valid"<< endl;
+        terminate();
+    }
+    cout <<a<< endl;
+    cout <<b<< endl;
+    cout <<c<< endl;
+    cout <<d<< endl;
+    cout <<e<< endl;
+    cout <<f<< endl;
+    cout <<g<< endl;
+    cout <<h<< endl;
+    cout <<i<< endl;
+    cout <<x<< endl;
+    cout <<"what task did u finish?(a b c d e f g h i x)"<< endl;
+    cin>>endig;
+    if (endig=="a")
+        a="finished";
+    else if (endig=="b")
+        b="finished";
+    else if (endig=="c")
+        c="finished";
+    else if (endig=="d")
+        d="finished";
+    else if (endig=="e")
+        e="finished";
+    else if (endig=="f")
+        f="finished";
+    else if (endig=="g")
+        g="finished";
+    else if (endig=="h")
+        h="finished";
+    else if (endig=="i")
+        i="finished";
+    else if (endig=="x")
+        x="finished";
+    else
+    {
+        cout<<"hmmmm that is not valid"<< endl;
+        terminate();
+    }
+    cout <<a<< endl;
+    cout <<b<< endl;
+    cout <<c<< endl;
+    cout <<d<< endl;
+    cout <<e<< endl;
+    cout <<f<< endl;
+    cout <<g<< endl;
+    cout <<h<< endl;
+    cout <<i<< endl;
+    cout <<x<< endl;
+    cout <<"what task did u finish?(a b c d e f g h i x)"<< endl;
+    cin>>endig;
+    if (endig=="a")
+        a="finished";
+    else if (endig=="b")
+        b="finished";
+    else if (endig=="c")
+        c="finished";
+    else if (endig=="d")
+        d="finished";
+    else if (endig=="e")
+        e="finished";
+    else if (endig=="f")
+        f="finished";
+    else if (endig=="g")
+        g="finished";
+    else if (endig=="h")
+        h="finished";
+    else if (endig=="i")
+        i="finished";
+    else if (endig=="x")
+        x="finished";
+    else
+    {
+        cout<<"hmmmm that is not valid"<< endl;
+        terminate();
+    }
+    cout <<a<< endl;
+    cout <<b<< endl;
+    cout <<c<< endl;
+    cout <<d<< endl;
+    cout <<e<< endl;
+    cout <<f<< endl;
+    cout <<g<< endl;
+    cout <<h<< endl;
+    cout <<i<< endl;
+    cout <<x<< endl;
+    cout <<"what task did u finish?(a b c d e f g h i x)"<< endl;
+    cin>>endig;
+    if (endig=="a")
+        a="finished";
+    else if (endig=="b")
+        b="finished";
+    else if (endig=="c")
+        c="finished";
+    else if (endig=="d")
+        d="finished";
+    else if (endig=="e")
+        e="finished";
+    else if (endig=="f")
+        f="finished";
+    else if (endig=="g")
+        g="finished";
+    else if (endig=="h")
+        h="finished";
+    else if (endig=="i")
+        i="finished";
+    else if (endig=="x")
+        x="finished";
+    else
+    {
+        cout<<"hmmmm that is not valid"<< endl;
+        terminate();
+    }
+    cout <<a<< endl;
+    cout <<b<< endl;
+    cout <<c<< endl;
+    cout <<d<< endl;
+    cout <<e<< endl;
+    cout <<f<< endl;
+    cout <<g<< endl;
+    cout <<h<< endl;
+    cout <<i<< endl;
+    cout <<x<< endl;
+    cout <<"what task did u finish?(a b c d e f g h i x)"<< endl;
+    cin>>endig;
+    if (endig=="a")
+        a="finished";
+    else if (endig=="b")
+        b="finished";
+    else if (endig=="c")
+        c="finished";
+    else if (endig=="d")
+        d="finished";
+    else if (endig=="e")
+        e="finished";
+    else if (endig=="f")
+        f="finished";
+    else if (endig=="g")
+        g="finished";
+    else if (endig=="h")
+        h="finished";
+    else if (endig=="i")
+        i="finished";
+    else if (endig=="x")
+        x="finished";
+    else
+    {
+        cout<<"hmmmm that is not valid"<< endl;
+        terminate();
+    }
+    cout <<a<< endl;
+    cout <<b<< endl;
+    cout <<c<< endl;
+    cout <<d<< endl;
+    cout <<e<< endl;
+    cout <<f<< endl;
+    cout <<g<< endl;
+    cout <<h<< endl;
+    cout <<i<< endl;
+    cout <<x<< endl;
+    cout <<"what task did u finish?(a b c d e f g h i x)"<< endl;
+    cin>>endig;
+    if (endig=="a")
+        a="finished";
+    else if (endig=="b")
+        b="finished";
+    else if (endig=="c")
+        c="finished";
+    else if (endig=="d")
+        d="finished";
+    else if (endig=="e")
+        e="finished";
+    else if (endig=="f")
+        f="finished";
+    else if (endig=="g")
+        g="finished";
+    else if (endig=="h")
+        h="finished";
+    else if (endig=="i")
+        i="finished";
+    else if (endig=="x")
+        x="finished";
+    else
+    {
+        cout<<"hmmmm that is not valid"<< endl;
+        terminate();
+    }
+    cout <<a<< endl;
+    cout <<b<< endl;
+    cout <<c<< endl;
+    cout <<d<< endl;
+    cout <<e<< endl;
+    cout <<f<< endl;
+    cout <<g<< endl;
+    cout <<h<< endl;
+    cout <<i<< endl;
+    cout <<x<< endl;
+    cout <<"what task did u finish?(a b c d e f g h i x)"<< endl;
+    cin>>endig;
+    if (endig=="a")
+        a="finished";
+    else if (endig=="b")
+        b="finished";
+    else if (endig=="c")
+        c="finished";
+    else if (endig=="d")
+        d="finished";
+    else if (endig=="e")
+        e="finished";
+    else if (endig=="f")
+        f="finished";
+    else if (endig=="g")
+        g="finished";
+    else if (endig=="h")
+        h="finished";
+    else if (endig=="i")
+        i="finished";
+    else if (endig=="x")
+        x="finished";
+    else
+    {
+        cout<<"hmmmm that is not valid"<< endl;
+        terminate();
+    }
+
+
+
+
+
+
+
+
+
+    return 0;
+}
